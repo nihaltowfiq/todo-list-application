0ec8f5c8608bf6f63522a85c11ce49736f542612
nihaltowfiq <nihaltowfiq@gmail.com>
2024-06-27 23:44:49 +0600

    updated

--- src/context/todos.tsx
+++ src/context/todos.tsx
@@ -10,50 +10,7 @@ import {
 	TodoState,
 } from './types';
 
-const initialState: TodoState = [
-	{
-		id: 1,
-		title: 'new 1 new 1 new 1 new 1 new 1 new 1',
-		description:
-			'description description description description description description description description description description ',
-		status: 'new',
-	},
-	{
-		id: 2,
-		title: 'new 2',
-		description:
-			'description description description description description description description description description description ',
-		status: 'new',
-	},
-	{
-		id: 3,
-		title: 'ongoing 1',
-		description:
-			'description description description description description description description description description description ',
-		status: 'ongoing',
-	},
-	{
-		id: 4,
-		title: 'ongoing 2',
-		description:
-			'description description description description description description description description description description ',
-		status: 'ongoing',
-	},
-	{
-		id: 5,
-		title: 'done 1',
-		description:
-			'description description description description description description description description description description ',
-		status: 'done',
-	},
-	{
-		id: 6,
-		title: 'done 2',
-		description:
-			'description description description description description description description description description description ',
-		status: 'done',
-	},
-];
+const initialState: TodoState = [];
 
 export const TodoContext = createContext<TodoContextProps>({
 	todos: initialState,
