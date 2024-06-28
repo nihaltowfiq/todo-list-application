249088be59f9d006bc0d8167eee782ca42b0f1d4
nihaltowfiq <nihaltowfiq@gmail.com>
2024-06-29 02:30:36 +0600

    gh-pages - deploying config added

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	base: '/todo-list-application/',
 	resolve: {
 		alias: {
 			'@libs': resolve(__dirname, 'src/libs'),
